ba373511c9846dfa6d51db1ad414bbe147fdcbcf
Christoffer <mech1803@gmail.com>
2025-09-10 15:55:12 +0200

    Add useFetch custom hook for data fetching; integrate into App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css';
+import { useFetch } from './hooks/useFetch';
+
 import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
+	const { data, error, isLoading } = useFetch(
+		'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m'
+	);
+
+	console.log(data, error, isLoading);
+
 	return (
 		<>
 			<header>
 				<Navbar />
 			</header>
 			<main>
-				<h1 className='title'>How's the sky looking today?</h1>
+				<h1 className="title">How's the sky looking today?</h1>
 			</main>
 		</>
 	);
---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react';
+
+// Custom hook to fetch data from a given URL
+export const useFetch = (url) => {
+	// State variables to hold data, error, and loading status
+	const [data, setData] = useState(null);
+	const [error, setError] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
+
+	// Effect to fetch data from the provided URL
+	useEffect(() => {
+		// AbortController to cancel fetch requests if the component unmounts
+		const controller = new AbortController();
+
+		// Options for the fetch request, including method, headers, and signal for aborting
+		const options = {
+			method: 'GET',
+			headers: {
+				accept: 'application/json',
+			},
+			signal: controller.signal,
+		};
+
+		// Function to fetch data from the URL
+		const fetchData = async () => {
+			try {
+				// Fetch data from the URL with the specified options
+				const response = await fetch(url, options);
+				// Check if the response is ok
+				if (!response.ok) {
+					setError('Failed to fetch data from the API');
+					throw new Error(
+						'Network response was not ok, please contact support if the issue persists.'
+					);
+				}
+				// Parse the response data as JSON
+				const data = await response.json();
+				// Update state with the fetched data and reset error
+				setData(data);
+				setError(null);
+			} catch (error) {
+                // Ignore abort errors
+				if (error.name === 'AbortError') {
+					return;
+				}
+				// If the fetch is aborted, ignore the error
+				setError(error.message);
+				throw new Error(error);
+			} finally {
+				// Set loading state to false after fetch completes, but only if not aborted to prevent flickering;
+				if (!controller.signal.aborted) {
+					setIsLoading(false);
+				}
+			}
+		};
+
+		// If a URL is provided, start fetching data
+		if (url) {
+			//  Set loading state to true before starting the fetch
+			setIsLoading(true);
+			fetchData();
+			// Cleanup function to abort the fetch request if the component unmounts
+			return () => {
+				controller.abort();
+			};
+		}
+	}, [url]);
+
+	// Return the fetched data, any error encountered, and the loading state
+	return { data, error, isLoading };
+};
